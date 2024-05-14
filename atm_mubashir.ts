00d79e4dd02800c24c5aca59a841dac49d30ea8c
mubashir <mubashirsono942@gmail.com>
2024-05-15 03:20:03 +0500

    first commit

---
+++ atm_mubashir.ts
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+interface ansType {
+    userId : string,
+    userPin : number,
+    accountType : string,
+    transactionsType : string,
+    amount : number
+
+}
+const answers : ansType = await inquirer.prompt([
+    {
+        name : "userId",
+        type : "input",
+        message : "kindly enter your ID :"
+    },
+    {
+        name : "userPin",
+        type : "number",
+        message : "kindly enter your PIN :"
+    },
+    {
+        name : "accountType",
+        type : "list",
+        message : "select your accountType :",
+        choices : ["current", "savings"]
+    },
+    {
+        name : "transactionsType",
+        type : "list",
+        message : "select your transactionsType :",
+        choices : ["fastCash", "withDraw"],
+        when(answers){
+            return answers.accountType
+        }
+    },
+    {
+        name : "amount",
+        type : "list",
+        message : "select your amount :",
+        choices : [1000, 5000 , 10000 , 20000 , 50000],
+        when(answers){
+            return answers.transactionsType == "fastCash"
+        }
+    },
+    {
+        name : "amount",
+        type : "number",
+        message : "insert your amount :",
+        when(answers){
+            return answers.transactionsType == "withDraw"
+        }
+    }
+])
+
+if(answers.userId && answers.userPin){
+    const balance = Math.floor(Math.random() *10000000)
+    console.log(balance);
+    const enteredAmount = answers.amount;
+    if(balance >= enteredAmount){
+       const remaining = balance - enteredAmount
+       console.log("your remaining balance is : ", remaining);
+    }else{
+        console.log("insufficient balance");
+    }
+}
+
+
